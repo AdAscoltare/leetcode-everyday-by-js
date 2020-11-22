57b75b32493c1f5e4fee361f30af0e0ede4e107a
AdAscoltare <736005057@qq.com>
2020-11-22 18:25:20 +0800

    2020/11/22 这题莫名其妙

---
+++ dynamic programming/E1025. Divisor Game.js
@@ -0,0 +1,25 @@
+/*
+ * @Descripttion:
+ * @version:
+ * @Author: Wang Wenzheng
+ * @Date: 2020-11-22 18:01:06
+ * @LastEditors: Wang Wenzheng
+ * @LastEditTime: 2020-11-22 18:22:44
+ */
+/**
+ * @param {number} N
+ * @return {boolean}
+ */
+var divisorGame = function (N) {
+  return N % 2 == 0;
+  //   if (N < 2) {
+  //     return false;
+  //   }
+  //   let res = Array(N + 1);
+  //   res[0] = false;
+  //   res[1] = false;
+  //   res[2] = true;
+  //   for(let i=3;i<=N;i++){
+
+  //   }
+};
